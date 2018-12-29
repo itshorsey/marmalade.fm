0ace5e3df3d7da594ec793c60cc58535cfd558d5
Jonathan Horsman <horsey@Jonathans-MacBook-Air.local>
2018-12-29 00:54:13 -0800

    Base project setup

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <div>
+            {/* Featured Mix */}
+            <div>
+              {/* Header */}
+              {/* Routed Page */}
+            </div>
+          </div>
+        </div>
+        {/* Audio Player */}
+      </div>
+    );
+  }
+}
+
+export default App;
